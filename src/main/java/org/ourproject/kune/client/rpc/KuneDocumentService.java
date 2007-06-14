c95a9ba4f4b0bb0121df7ce27ab3f6e3d8de0152
vjrj <vjrj@a678e951-6d2c-0410-b835-ed4c891391b6>
2007-06-14 23:12:26 +0000

    some comments

--- src/main/java/org/ourproject/kune/client/rpc/KuneDocumentService.java
+++ src/main/java/org/ourproject/kune/client/rpc/KuneDocumentService.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2007 The kune development team (see CREDITS for details)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 dated June, 1991.
+ * 
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
 package org.ourproject.kune.client.rpc;
 
 import org.ourproject.kune.client.rpc.dto.KuneDoc;
@@ -6,7 +24,12 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
-public interface KuneDocumentService extends RemoteService{
+/**
+ * 
+ * @author Daniel Gomez
+ *
+ */
+public interface KuneDocumentService extends RemoteService {
 
 	public KuneDoc getRootDocument(String projectName);
 	
--- src/main/java/org/ourproject/kune/client/rpc/KuneDocumentServiceAsync.java
+++ src/main/java/org/ourproject/kune/client/rpc/KuneDocumentServiceAsync.java
@@ -1,7 +1,30 @@
+/*
+ * Copyright (C) 2007 The kune development team (see CREDITS for details)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 dated June, 1991.
+ * 
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
 package org.ourproject.kune.client.rpc;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+/**
+ * 
+ * @author Daniel Gomez
+ *
+ */
 public interface KuneDocumentServiceAsync {
 	public void getRootDocument(String projectName, AsyncCallback callback);
 }
