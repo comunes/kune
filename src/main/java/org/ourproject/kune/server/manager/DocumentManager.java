2c96258ce7a9f77a8eb022456184707744c61daa
danigb <danigb@a678e951-6d2c-0410-b835-ed4c891391b6>
2007-06-15 16:22:37 +0000

    refactoring to DAO

--- src/main/java/org/ourproject/kune/server/manager/DocumentManager.java
+++ src/main/java/org/ourproject/kune/server/manager/DocumentManager.java
@@ -1,24 +1,23 @@
 package org.ourproject.kune.server.manager;
 
-import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
 import org.ourproject.kune.client.rpc.dto.KuneDoc;
+import org.ourproject.kune.server.dao.DocumentDao;
 
 public class DocumentManager {
-    public KuneDoc readRootDocument(Session session) throws RepositoryException {
-	Node rootDocument = session.getRootNode().getNode("documents").getNode("root");
-	String title = rootDocument.getProperty("title").getValue().getString();
-	String content = rootDocument.getProperty("content").getValue().getString();
-	String license = rootDocument.getProperty("license_name").getValue().getString();
-	return new KuneDoc(title, content, license);
+    private final DocumentDao documentDao;
+
+    public DocumentManager() {
+	documentDao = new DocumentDao();
     }
 
-    public void saveDocument(KuneDoc doc, Session theSess) throws RepositoryException {
-	Node rootDocument = theSess.getRootNode().getNode("documents").getNode("root");
-	rootDocument.setProperty("title", doc.getName());
-	rootDocument.setProperty("content", doc.getContent());
-	rootDocument.setProperty("license_name", doc.getLicenseName());
+    public KuneDoc readRootDocument(Session session) throws RepositoryException {
+	return documentDao.getRoot(session);
+    }
+
+    public void saveDocument(KuneDoc doc, Session session) throws RepositoryException {
+	documentDao.saveDocument(session, doc);
     }
 }
