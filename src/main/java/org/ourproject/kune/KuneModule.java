f39afccc9c91731eb77c4003ac2a015aeeac2ee1
danigb <danigb@a678e951-6d2c-0410-b835-ed4c891391b6>
2007-06-20 13:41:35 +0000

    log interceptor

--- src/main/java/org/ourproject/kune/KuneModule.java
+++ src/main/java/org/ourproject/kune/KuneModule.java
@@ -1,15 +1,18 @@
 package org.ourproject.kune;
 
+import org.ourproject.kune.log.LoggerMethodInterceptor;
 import org.ourproject.kune.server.dao.DocumentDao;
 import org.ourproject.kune.server.dao.DocumentDaoJCR;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.matcher.Matchers;
 
 public class KuneModule extends AbstractModule {
 
     @Override
     protected void configure() {
 	bind(DocumentDao.class).to(DocumentDaoJCR.class);
+	bindInterceptor(Matchers.any(), Matchers.any(), new LoggerMethodInterceptor());
     }
 
 }
