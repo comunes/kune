ea5392d0d6636739e8da102f0d51c5f6522b76f9
danigb <danigb@a678e951-6d2c-0410-b835-ed4c891391b6>
2007-12-02 21:42:46 +0000

    json web services!

--- src/main/java/org/ourproject/kune/platf/server/rest/GroupJSONService.java
+++ src/main/java/org/ourproject/kune/platf/server/rest/GroupJSONService.java
@@ -2,27 +2,33 @@ package org.ourproject.kune.platf.server.rest;
 
 import java.util.List;
 
+import org.ourproject.kune.platf.client.dto.LinkDTO;
 import org.ourproject.kune.platf.server.domain.Group;
 import org.ourproject.kune.platf.server.manager.GroupManager;
+import org.ourproject.kune.platf.server.mapper.Mapper;
 import org.ourproject.rack.filters.rest.REST;
 
 import com.google.inject.Inject;
 
 public class GroupJSONService {
 	private final GroupManager manager;
+	private final Mapper mapper;
 
 	@Inject
-	public GroupJSONService(GroupManager manager) {
+	public GroupJSONService(GroupManager manager, Mapper mapper) {
 		this.manager = manager;
+		this.mapper = mapper;
 	}
 
 	@REST(params={"query"})
-	public List<Group> search(String search) {
-		return manager.search(search);
+	public List<LinkDTO> search(String search) {
+		List<Group> results = manager.search(search);
+		return mapper.mapList(results, LinkDTO.class);
 	}
 
 	@REST(params={"query", "fisrt", "max"})
-	public List<Group> search(String search, Integer firstResult, Integer maxResults) {
-		return manager.search(search, firstResult, maxResults);
+	public List<LinkDTO> search(String search, Integer firstResult, Integer maxResults) {
+		List<Group> results = manager.search(search, firstResult, maxResults);
+		return mapper.mapList(results, LinkDTO.class);
 	}
 }
