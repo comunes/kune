7ba9b77c890290b893c52f1e14af949640fa5021
vjrj <vjrj@a678e951-6d2c-0410-b835-ed4c891391b6>
2007-12-20 08:37:01 +0000

    Liquibase custom rollbacks

---
+++ scripts/liquibase-migrate.sh
@@ -0,0 +1,12 @@
+java -jar $M2_REPO/org/liquibase/liquibase-core/1.4.1/liquibase-core-1.4.1.jar \
+      --driver=com.mysql.jdbc.Driver \
+      --classpath=/home/vjrj/.m2/repository/mysql/mysql-connector-java/5.0.5/mysql-connector-java-5.0.5.jar \
+      --changeLogFile=src/main/resources/db/liquibase_changelog.xml \
+      --url="jdbc:mysql://localhost/kune_dev" \
+      --username=kune \
+      --password=db4kune \
+      migrate
+
+#java -jar $M2_REPO/org/liquibase/liquibase-core/1.4.1/liquibase-core-1.4.1.jar \
+#  --defaultsFile=src/main/resources/db/liquibase.properties \
+#  migrate
