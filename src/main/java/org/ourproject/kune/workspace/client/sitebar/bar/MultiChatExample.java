fe397d7a16ceaf0c275d2fd8e50e41a166cdc080
vjrj <vjrj@a678e951-6d2c-0410-b835-ed4c891391b6>
2008-03-21 06:13:26 +0000

    Refactorization for plugins and UI organization

---
+++ src/main/java/org/ourproject/kune/workspace/client/sitebar/bar/MultiChatExample.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2007 The kune development team (see CREDITS for details)
+ * This file is part of kune.
+ *
+ * Kune is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kune is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.ourproject.kune.workspace.client.sitebar.bar;
+
+import org.ourproject.kune.chat.client.ChatFactory;
+import org.ourproject.kune.chat.client.rooms.MultiRoom;
+import org.ourproject.kune.chat.client.rooms.MultiRoomListener;
+import org.ourproject.kune.chat.client.rooms.Room;
+import org.ourproject.kune.chat.client.rooms.RoomUser;
+
+public class MultiChatExample {
+
+    public static void show() {
+        final MultiRoom rooms = ChatFactory.createChatMultiRoom(new MultiRoomListener() {
+            public void onSendMessage(Room room, String message) {
+                room.addMessage(room.getSessionAlias(), message);
+            }
+
+            public void onCloseRoom(Room room) {
+            }
+        });
+        rooms.show();
+
+        Room room1 = rooms.createRoom("chat1@talks.localhost", "luther.b", RoomUser.PARTICIPANT);
+        room1.setSubject("Welcome to chat1, today topic: Cultural issues in Brazil");
+        room1.addUser("otro usuario", RoomUser.MODERADOR);
+        room1.addMessage("luther.b", "Mensaje de test en room1");
+
+        Room room2 = rooms.createRoom("chat2@talks.localhost", "luther", RoomUser.PARTICIPANT);
+        room2.setSubject("Welcome to this room: we are talking today about 2009 meeting");
+        room2.addUser("luther", RoomUser.MODERADOR);
+        room2.addMessage("luther", "Mensaje de test en room2");
+        room2.addInfoMessage("Mensaje de evento en room2");
+        room2.addDelimiter("17:35");
+
+    }
+}
---
+++ src/main/java/org/ourproject/kune/workspace/client/sitebar/rpc/SiteBarServiceMocked.java
@@ -0,0 +1,68 @@
+/*
+ *
+ * Copyright (C) 2007 The kune development team (see CREDITS for details)
+ * This file is part of kune.
+ *
+ * Kune is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kune is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.ourproject.kune.workspace.client.sitebar.rpc;
+
+import org.ourproject.kune.platf.client.dto.UserDTO;
+import org.ourproject.kune.platf.client.dto.UserInfoDTO;
+import org.ourproject.kune.workspace.client.sitebar.Site;
+
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public class SiteBarServiceMocked implements UserServiceAsync {
+
+    public void login(final String nick, final String pass, final AsyncCallback<UserInfoDTO> callback) {
+        Site.showProgress("Login");
+        Timer timer = new Timer() {
+            public void run() {
+                Site.hideProgress();
+                callback.onSuccess(new UserInfoDTO());
+            }
+        };
+        timer.schedule(1000);
+    }
+
+    public void logout(final String userHash, final AsyncCallback<?> callback) {
+        timerAndSuccess(callback);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void timerAndSuccess(final AsyncCallback callback) {
+        Timer timer = new Timer() {
+            public void run() {
+                callback.onSuccess(null);
+            }
+        };
+        timer.schedule(1000);
+    }
+
+    public void createUser(final UserDTO user, final AsyncCallback<UserInfoDTO> asyncCallback) {
+        timerAndSuccess(asyncCallback);
+    }
+
+    public void reloadUserInfo(final String userHash, final AsyncCallback<UserInfoDTO> asyncCallback) {
+        timerAndSuccess(asyncCallback);
+    }
+
+    public void onlyCheckSession(final String userHash, final AsyncCallback<?> asyncCallback) {
+        timerAndSuccess(asyncCallback);
+    }
+}
