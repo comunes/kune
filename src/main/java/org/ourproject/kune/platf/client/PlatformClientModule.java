62c9cace98e8194c48cbcad2de7dc94e8f69e548
vjrj <vjrj@a678e951-6d2c-0410-b835-ed4c891391b6>
2008-03-21 03:30:11 +0000

    Refactorization for plugins and UI organization

---
+++ src/main/java/org/ourproject/kune/platf/client/PlatformClientModule.java
@@ -0,0 +1,35 @@
+/*
+ *
+ * Copyright (C) 2007 The kune development team (see CREDITS for details)
+ * This file is part of kune.
+ *
+ * Kune is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kune is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.ourproject.kune.platf.client;
+
+import org.ourproject.kune.platf.client.extend.ClientModule;
+import org.ourproject.kune.platf.client.extend.Register;
+import org.ourproject.kune.workspace.client.actions.AttachToExtensionPointAction;
+import org.ourproject.kune.workspace.client.actions.ClearExtensionPointAction;
+import org.ourproject.kune.workspace.client.actions.DetachFromExtensionPointAction;
+
+public class PlatformClientModule implements ClientModule {
+    public void configure(final Register register) {
+        register.addAction(PlatformEvents.ATTACH_TO_EXT_POINT, new AttachToExtensionPointAction());
+        register.addAction(PlatformEvents.DETACH_FROM_EXT_POINT, new DetachFromExtensionPointAction());
+        register.addAction(PlatformEvents.CLEAR_EXT_POINT, new ClearExtensionPointAction());
+    }
+}
---
+++ src/main/java/org/ourproject/kune/platf/client/PlatformEvents.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2007 The kune development team (see CREDITS for details)
+ * This file is part of kune.
+ *
+ * Kune is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kune is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.ourproject.kune.platf.client;
+
+public interface PlatformEvents {
+    public static final String ATTACH_TO_EXT_POINT = "ws.AttachToExtensionPoint";
+    public static final String DETACH_FROM_EXT_POINT = "ws.DetachToExtensionPoint";
+    public static final String CLEAR_EXT_POINT = "ws.ClearExtensionPoint";
+}
