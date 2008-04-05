f97050ef9996a4767bfdf33cd99c9bbd904c5e9d
vjrj <vjrj@a678e951-6d2c-0410-b835-ed4c891391b6>
2008-04-05 00:06:53 +0000

    Copyright update to Affero

--- src/main/java/org/ourproject/kune/platf/client/ui/palette/WebSafePalettePanel.java
+++ src/main/java/org/ourproject/kune/platf/client/ui/palette/WebSafePalettePanel.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (C) 2007 The kune development team (see CREDITS for details)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 dated June, 1991.
- *
- * This package is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- *
- */
-
 package org.ourproject.kune.platf.client.ui.palette;
 
 import com.google.gwt.user.client.DOM;
@@ -35,37 +17,37 @@ public class WebSafePalettePanel extends Composite implements WebSafePaletteView
     private final WebSafePalettePresenter presenter;
 
     public WebSafePalettePanel(final WebSafePalettePresenter initPresenter) {
-	this.presenter = initPresenter;
+        this.presenter = initPresenter;
 
-	Grid paletteGrid = new Grid(ROWS, COLS);
+        Grid paletteGrid = new Grid(ROWS, COLS);
 
-	initWidget(paletteGrid);
+        initWidget(paletteGrid);
 
-	paletteGrid.setCellSpacing(1);
-	// Put color values in the grid cells
+        paletteGrid.setCellSpacing(1);
+        // Put color values in the grid cells
 
-	int row;
-	int col;
-	int n = 0;
-	for (int a = 0; a < COLORS.length; a++) {
-	    for (int b = 0; b < COLORS.length; b++) {
-		for (int c = 0; c < COLORS.length; c++) {
-		    row = n / COLS;
-		    col = n % COLS;
-		    String currentColor = "#" + COLORS[c] + COLORS[a] + COLORS[b];
-		    paletteGrid.setText(row, col, " ");
-		    DOM.setStyleAttribute(paletteGrid.getCellFormatter().getElement(row, col), "backgroundColor",
-			    currentColor);
-		    n++;
-		}
-	    }
-	}
-	paletteGrid.addStyleName("kune-WebSafePalette");
-	paletteGrid.addTableListener(new TableListener() {
-	    public void onCellClicked(final SourcesTableEvents sender, final int row, final int col) {
-		presenter.onColorSelected(row, col);
-	    }
-	});
+        int row;
+        int col;
+        int n = 0;
+        for (int a = 0; a < COLORS.length; a++) {
+            for (int b = 0; b < COLORS.length; b++) {
+                for (int c = 0; c < COLORS.length; c++) {
+                    row = n / COLS;
+                    col = n % COLS;
+                    String currentColor = "#" + COLORS[c] + COLORS[a] + COLORS[b];
+                    paletteGrid.setText(row, col, " ");
+                    DOM.setStyleAttribute(paletteGrid.getCellFormatter().getElement(row, col), "backgroundColor",
+                            currentColor);
+                    n++;
+                }
+            }
+        }
+        paletteGrid.addStyleName("kune-WebSafePalette");
+        paletteGrid.addTableListener(new TableListener() {
+            public void onCellClicked(final SourcesTableEvents sender, final int row, final int col) {
+                presenter.onColorSelected(row, col);
+            }
+        });
     }
 
 }
--- src/main/java/org/ourproject/kune/platf/client/ui/rate/RatePanel.java
+++ src/main/java/org/ourproject/kune/platf/client/ui/rate/RatePanel.java
@@ -1,23 +1,7 @@
-/*
- * Copyright (C) 2007 The kune development team (see CREDITS for details)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 dated June, 1991.
- *
- * This package is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- *
- */
 package org.ourproject.kune.platf.client.ui.rate;
 
 import org.ourproject.kune.platf.client.services.Kune;
+
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Image;
