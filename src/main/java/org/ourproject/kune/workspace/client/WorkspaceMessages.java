19fec7b37ff4339ec7414988220b65d3d2f7ac47
shevek <shevek@a678e951-6d2c-0410-b835-ed4c891391b6>
2008-12-05 16:22:07 +0000

    Translation improved

--- src/main/java/org/ourproject/kune/workspace/client/WorkspaceMessages.java
+++ src/main/java/org/ourproject/kune/workspace/client/WorkspaceMessages.java
@@ -2,7 +2,7 @@ package org.ourproject.kune.workspace.client;
 
 public interface WorkspaceMessages {
     String REGISTER_A_NEW_GROUP_TITLE = "Register a new Group";
-    String MUST_BE_BETWEEN_3_AND_15 = "Must be between 3 and 15 lowercase characters. Can only contain occidental characters, numbers, and dashes";
-    String NAME_IN_ALREADY_IN_USE = "This name in already in use, try with a different name.";
-    String REGISTER_TO_CREATE_A_GROUP = "Sign in or register to create a group";
+    String MUST_BE_BETWEEN_3_AND_15 = "The name must be between 3 and 15 lowercase characters. It can only contain Western characters, numbers, and dashes";
+    String NAME_IN_ALREADY_IN_USE = "This name is already in use. Please try with a different name";
+    String REGISTER_TO_CREATE_A_GROUP = "Please, sign in or register to create a group";
 }
