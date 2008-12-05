ede668eea58c23430559c7aa203808e56a120c93
shevek <shevek@a678e951-6d2c-0410-b835-ed4c891391b6>
2008-12-05 16:21:02 +0000

    Translation improved

--- src/main/java/org/ourproject/kune/platf/client/PlatfMessages.java
+++ src/main/java/org/ourproject/kune/platf/client/PlatfMessages.java
@@ -5,11 +5,11 @@ public interface PlatfMessages {
     String MEMBERS_NOT_PUBLIC = "The members of this group are not public";
     String REGISTER_TITLE = "Register";
     String SIGN_IN_TITLE = "Sign in";
-    String EMAIL_IN_USE = "This email in in use by other person, try with another.";
-    String NAME_IN_USE = "This name in already in use, try with a different name.";
-    String INCORRECT_NICKNAME_EMAIL_OR_PASSWORD = "Incorrect nickname/email or password";
+    String EMAIL_IN_USE = "This email address is being used by another user. Please, try with another one";
+    String NAME_IN_USE = "This name in already in use. Please try with a different name";
+    String INCORRECT_NICKNAME_EMAIL_OR_PASSWORD = "The nickname/email or password is incorrect";
     String YOUR_HOMEPAGE = "Your homepage";
-    String USER_DON_T_HAVE_A_HOMEPAGE = "This user don't have a homepage";
+    String USER_DON_T_HAVE_A_HOMEPAGE = "This user does not have a homepage";
     String ENT_LOGO_SELECTOR_NORMAL_TITLE = "Logo";
     String ENT_LOGO_SELECTOR_PERSON_TITLE = "Avatar";
     String ENT_OPTIONS_GROUP_TITLE = "Group options";
