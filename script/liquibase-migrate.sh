b501e99dd8df391a6e3b3172d8e8095c4efc6807
vjrj_ <vjrj_@a678e951-6d2c-0410-b835-ed4c891391b6>
2011-03-13 20:53:28 +0000

    liquibase script updated

--- script/liquibase-migrate.sh
+++ script/liquibase-migrate.sh
@@ -1,4 +1,5 @@
-java -jar $M2_REPO/org/liquibase/liquibase-core/1.4.1/liquibase-core-1.4.1.jar \
+. script/mvn-vars.sh
+java -jar $M2_REPO/org/liquibase/liquibase-core/1.7.0/liquibase-core-1.7.0.jar \
       --driver=com.mysql.jdbc.Driver \
       --classpath=$M2_REPO/mysql/mysql-connector-java/5.0.5/mysql-connector-java-5.0.5.jar \
       --changeLogFile=src/main/resources/db/liquibase_changelog.xml \
