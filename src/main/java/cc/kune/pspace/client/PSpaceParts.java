f0dcde37ca15a88abf7fdbaef8266922a1850e3f
vjrj_ <vjrj_@a678e951-6d2c-0410-b835-ed4c891391b6>
2011-03-25 20:22:13 +0000

    server refactorization

---
+++ src/main/java/cc/kune/pspace/client/PSpaceParts.java
@@ -0,0 +1,21 @@
+package cc.kune.pspace.client;
+
+import cc.kune.core.client.init.AppStartEvent;
+import cc.kune.core.client.init.AppStartEvent.AppStartHandler;
+import cc.kune.core.client.state.Session;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+public class PSpaceParts {
+
+    @Inject
+    public PSpaceParts(final Session session, final Provider<PSpacePresenter> pspacePresenter) {
+        session.onInitDataReceived(true, new AppStartHandler() {
+            @Override
+            public void onAppStart(final AppStartEvent event) {
+                pspacePresenter.get();
+            }
+        });
+    }
+}
