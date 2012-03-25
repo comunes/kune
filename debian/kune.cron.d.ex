4db877a4f029263e160e6a26065bfb269e70d30b
vjrj_ <vjrj_@a678e951-6d2c-0410-b835-ed4c891391b6>
2012-03-25 21:42:33 +0000

    debian package

---
+++ debian/kune.cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the kune package
+#
+#0 4	* * *	root	[ -x /usr/bin/kune_maintenance ] && /usr/bin/kune_maintenance
---
+++ debian/kune.default.ex
@@ -0,0 +1,10 @@
+# Defaults for kune initscript
+# sourced by /etc/init.d/kune
+# installed at /etc/default/kune by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
