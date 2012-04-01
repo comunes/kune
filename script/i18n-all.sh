edd4017faa37cbc06d0adb36056befe88428fe98
vjrj_ <vjrj_@a678e951-6d2c-0410-b835-ed4c891391b6>
2012-04-01 13:47:28 +0000

    Added some new languages and added some maven profiles (-P development or -P production) to make development faster (gwt compilation in fact)

---
+++ script/i18n-all.sh
@@ -0,0 +1,6 @@
+bin/i18n-db2gwt.sh -l en -j > KuneConstants.java
+bin/i18n-db2gwt.sh -l en > KuneConstants.properties
+bin/i18n-db2gwt.sh -l es > KuneConstants_es.properties
+bin/i18n-db2gwt.sh -l fr > KuneConstants_fr.properties
+bin/i18n-db2gwt.sh -l ca > KuneConstants_ca.properties
+bin/i18n-db2gwt.sh -l it > KuneConstants_it.properties
