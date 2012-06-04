2bcbdc22aabd60b1ae5f3e5c9dd382be4efb5063
Vicente J. Ruiz Jurado <vjrj@ourproject.org>
2012-06-04 10:16:22 +0200

    About updated

--- script/generateAbout.sh
+++ script/generateAbout.sh
@@ -7,15 +7,13 @@ fi
 trap "rm -rf $tmp" 1 2 3 9 15
 
 
-REV=`svn info --xml| grep -m 1 revi | cut -d\" -f 2`
-COMMITSPENDING=`svn status | grep -c ""`
-SVNVER=r$REV+c$COMMITSPENDING
 VER=`grep "<version>" pom.xml | head -1 | sed 's/..version.//g'`
+GITVER=`git rev-parse HEAD`
 
 
 #echo "kune" >> $tmp
 #echo "--------------------------------------------------------------------------------" >> $tmp
-echo Version $VER '('$SVNVER')' >> $tmp
+echo Version $VER '('$GITVER')' >> $tmp
 
 txt2html --prepend_file script/header.html -h 80 --preserve_indent -8 --style_url frame-def.css --infile $tmp --infile CREDITS --infile COPYRIGHT --outfile src/main/java/cc/kune/core/public/about.html
 rm $tmp
