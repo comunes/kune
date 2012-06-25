31fde6826f60c00b18beb1d052e24efe7c55354a
Vicente J. Ruiz Jurado <vjrj@ourproject.org>
2012-06-25 11:38:59 +0200

    Moving img to https://gitorious.org/kune/artwork

--- img/icons/convert-gif.sh
+++
@@ -1,4 +0,0 @@
-convert arrow-down-black.png arrow-down-black.gif
-convert arrow-down-white.png arrow-down-white.gif
-convert arrow-right-black.png arrow-right-black.gif
-convert arrow-right-white.png arrow-right-white.gif
