6b85ad6cdd6cd1b8c5a4b54e94de5dbcf6c39e0e
Vicente J. Ruiz Jurado <vjrj@ourproject.org>
2012-09-12 00:01:35 +0200

    Added a diff liquibase script

---
+++ bin/liquibase-diffChangeLog.sh
@@ -0,0 +1,13 @@
+. bin/mvn-vars.sh
+java -jar $M2_REPO/org/liquibase/liquibase-core/1.8.1/liquibase-core-1.8.1.jar \
+      --driver=com.mysql.jdbc.Driver \
+      --classpath=$M2_REPO/mysql/mysql-connector-java/5.1.13/mysql-connector-java-5.1.13.jar \
+      --url=jdbc:mysql://localhost/kune_dev \
+      --username=kune \
+      --password=db4kune \
+      diffChangeLog \
+      --baseUrl=jdbc:mysql://localhost/kune_prod \
+      --baseUsername=kune \
+      --basePassword=db4kune
+# later version referenceUrl, etc
+# diff instead of diffChangeLog
