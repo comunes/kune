66b078f01b1bb61331aaede03c069957703ef382
Vicente J. Ruiz Jurado <vjrj@ourproject.org>
2012-09-19 23:30:55 +0200

    New clearcheckums liquibase script

---
+++ bin/liquibase-clearChecksums.sh
@@ -0,0 +1,13 @@
+. bin/mvn-vars.sh
+java -jar $M2_REPO/org/liquibase/liquibase-core/1.8.1/liquibase-core-1.8.1.jar \
+      --driver=com.mysql.jdbc.Driver \
+      --classpath=$M2_REPO/mysql/mysql-connector-java/5.1.13/mysql-connector-java-5.1.13.jar \
+      --changeLogFile=src/main/resources/db/liquibase_changelog.xml \
+      --url="jdbc:mysql://localhost/kune_dev" \
+      --username=kune \
+      --password=db4kune \
+      clearCheckSums
+
+#java -jar $M2_REPO/org/liquibase/liquibase-core/1.4.1/liquibase-core-1.4.1.jar \
+#  --defaultsFile=src/main/resources/db/liquibase.properties \
+#  migrate
