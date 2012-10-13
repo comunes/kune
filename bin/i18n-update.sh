6ea747ea03a3b2a777ff008e987960845eb52721
vjrj <vjrj@ourproject.org>
2012-10-13 11:47:07 +0200

    Updated languages, added Polish

---
+++ bin/i18n-update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+HOST=kune@kune.beta.iepala.es
+#ssh $HOST "cd /home/kune/kune-current/; bin/i18n-all.ssh"
+scp $HOST:/home/kune/kune-current/KuneCon* src/main/java/cc/kune/core/client/i18n/
