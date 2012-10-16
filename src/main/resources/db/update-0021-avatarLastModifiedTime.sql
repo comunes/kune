17f59c5fd59950539eca862aab64398db8c59ace
vjrj <vjrj@ourproject.org>
2012-10-16 14:23:06 +0200

    Some more fixes for initialization

--- src/main/resources/db/update-0021-avatarLastModifiedTime.sql
+++ src/main/resources/db/update-0021-avatarLastModifiedTime.sql
@@ -1,1 +1,22 @@
-ALTER TABLE groups ADD COLUMN logoLastModifiedTime BIGINT NOT NULL DEFAULT 1347400051999;
+-- Based in http://stackoverflow.com/questions/10134044/mysql-stored-procedure-executing-show-create-table
+delimiter '//'
+
+CREATE PROCEDURE addcol() BEGIN
+SET @dbName = (SELECT DATABASE());
+IF NOT EXISTS(
+	SELECT * FROM information_schema.COLUMNS
+	WHERE COLUMN_NAME='logoLastModifiedTime' AND TABLE_NAME='groups' AND TABLE_SCHEMA=@dbName
+	)
+	THEN
+		ALTER TABLE `groups`
+		ADD COLUMN `logoLastModifiedTime` BIGINT NOT NULL DEFAULT 1347400051999;
+
+END IF;
+END;
+//
+
+delimiter ';'
+
+CALL addcol();
+
+DROP PROCEDURE addcol;
