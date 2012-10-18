8b6d733c1e6d38a44ccce2ca0e5f9ab3c1f25b15
vjrj <vjrj@ourproject.org>
2012-10-18 21:53:34 +0200

    Updated languages and credits

--- bin/i18n-all.sh
+++ bin/i18n-all.sh
@@ -4,5 +4,12 @@ bin/i18n-db2gwt.sh -l es > KuneConstants_es.properties
 bin/i18n-db2gwt.sh -l fr > KuneConstants_fr.properties
 bin/i18n-db2gwt.sh -l ca > KuneConstants_ca.properties
 bin/i18n-db2gwt.sh -l it > KuneConstants_it.properties
-bin/i18n-db2gwt.sh -l ru > KuneConstants_ru.properties
+bin/i18n-db2gwt.sh -l ro > KuneConstants_ro.properties
+bin/i18n-db2gwt.sh -l tr > KuneConstants_tr.properties
 bin/i18n-db2gwt.sh -l de > KuneConstants_de.properties
+bin/i18n-db2gwt.sh -l ar > KuneConstants_ar.properties
+bin/i18n-db2gwt.sh -l ru > KuneConstants_ru.properties
+bin/i18n-db2gwt.sh -l hu > KuneConstants_hu.properties
+bin/i18n-db2gwt.sh -l pl > KuneConstants_pl.properties
+bin/i18n-db2gwt.sh -l gl > KuneConstants_gl.properties
+bin/i18n-db2gwt.sh -l eu > KuneConstants_eu.properties
