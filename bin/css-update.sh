73cf26c7fe81c9427093647ce918cb989d170d9d
vjrj <vjrj@ourproject.org>
2012-11-01 14:28:07 +0100

    css-update fix after refactorization

--- bin/css-update.sh
+++ bin/css-update.sh
@@ -13,7 +13,7 @@ APPPUB=src/main/java/org/ourproject/kune/app/public
 
 cp src/main/java/cc/kune/chat/public/kune-chat.css target/kune-$KUNE_VERSION/ws/
 cp src/main/java/cc/kune/chat/public/kune-hablar.css target/kune-$KUNE_VERSION/ws/
-cp src/main/java/cc/kune/msgs/public/kune-message.css target/kune-$KUNE_VERSION/ws/
+cp src/main/java/cc/kune/common/public/kune-message.css target/kune-$KUNE_VERSION/ws/
 cp src/main/java/cc/kune/common/public/kune-common.css target/kune-$KUNE_VERSION/ws/
 cp src/main/java/cc/kune/common/public/kune-custom-common.css target/kune-$KUNE_VERSION/ws/
 cp src/main/java/cc/kune/core/public/ws.css target/kune-$KUNE_VERSION/ws/
@@ -21,4 +21,4 @@ cp src/main/java/cc/kune/core/public/ws-rtl.css target/kune-$KUNE_VERSION/ws/
 #cp src/main/java/cc/kune/core/public/ws.html target/kune-$KUNE_VERSION/
 #cp src/main/webapp/templates/basic/basic.css  target/kune-$KUNE_VERSION/templates/basic/basic.css 
 #cp src/main/webapp/templates/basic/docs.liquid.html  target/kune-$KUNE_VERSION/templates/basic/
-cp src/main/java/cc/kune/common/public/gxt-custom/css/gxt-op-common.css target/kune-$KUNE_VERSION/ws/gxt-custom/css/gxt-op-common.css
+cp src/main/java/cc/kune/gxtbinds/public/gxt-custom/css/gxt-op-common.css target/kune-$KUNE_VERSION/ws/gxt-custom/css/gxt-op-common.css
