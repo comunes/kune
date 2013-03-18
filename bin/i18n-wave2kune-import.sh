78d1215a9be1816ebc0ee6f876c653d04eae550f
vjrj <vjrj@sanuk.(none)>
2013-03-18 12:18:22 +0100

    i18n wave import updated (wip)

--- bin/i18n-wave2kune-import.sh
+++ bin/i18n-wave2kune-import.sh
@@ -1,6 +1,63 @@
 #!/bin/bash
-for i in `find src -name *Mess*_en.properties | tail -5` ; 
+
+usage() {
+    echo "Use: $0 -u kune_db_username -p kune_db_passwd -d kune_db_name"
+}
+
+USERNAME="kune_prod"
+PASS="db4kune"
+DB="kune_prod"
+
+while getopts “hu:p:d:” OPTION
+do
+    case $OPTION in
+	h)
+            usage
+            exit 1
+            ;;
+	u)
+	    USERNAME=$OPTARG
+	    ;;
+	p)
+	    PASS=$OPTARG
+	    ;;
+	d)
+	    DB=$OPTARG
+	    ;;   
+	?)
+            usage
+            exit
+            ;;
+    esac
+done	
+
+if [[ -z $PASS || -z $USERNAME || -z $DB ]]
+then
+    usage
+    exit 1
+fi
+
+
+if [[ -z $WAVE_HOME ]]
+then 
+  echo "Please define were the Wave code is located using 'export WAVE_HOME=/some/path'"
+  exit 1
+fi
+
+DELIMITER="|"
+
+for i in `find $WAVE_HOME/src -name *Mess*_en.properties | tail -5` ; 
 do 
-  FILE=`echo $i |sed 's/\//\./g'`
-  cat $i | sed -e '$a\' | sed '/^[\b]*$/d' | sed "s/^/$FILE|/g" | sed '0,/RE/s/ = /|/g' ; 
-done
+  cat $i | \
+  # Add newline at the end (if I rember)
+  sed -e '$a\' | \
+  # Remove empty lines  
+  sed '/^[\b]*$/d' | \
+  # Add file name key to the start (escaping /)
+  sed "s/^/`echo $i | sed 's/\//\\\\\//g'`|/g" | \
+  # Substitute first " = " with delimiter
+  sed "0,/RE/s/ = /$DELIMITER/g" ; 
+done | awk -F "|" '{
+  select="mysql -p"$PASSWD" -u"$USERNAME" "$DB" < SELECT COUNT(*) FROM globalize_translations g WHERE gtype="$2;
+  print select;
+}'
