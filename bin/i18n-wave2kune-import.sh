ca757c1646e6f7d6b8dbb4f8be09bebd22e6a8cc
vjrj <vjrj@ourproject.org>
2013-03-19 19:36:58 +0100

    More work in i18n-wave2kune-import

--- bin/i18n-wave2kune-import.sh
+++ bin/i18n-wave2kune-import.sh
@@ -4,9 +4,10 @@ usage() {
     echo "Use: $0 -u kune_db_username -p kune_db_passwd -d kune_db_name"
 }
 
-USERNAME="kune_prod"
+USERNAME="kune"
 PASS="db4kune"
-DB="kune_prod"
+DB="kune_dev"
+WAVE_HOME=/home/vjrj/dev/wave/
 
 while getopts “hu:p:d:” OPTION
 do
@@ -57,7 +58,21 @@ do
   sed "s/^/`echo $i | sed 's/\//\\\\\//g'`|/g" | \
   # Substitute first " = " with delimiter
   sed "0,/RE/s/ = /$DELIMITER/g" ; 
-done | awk -F "|" '{
-  select="mysql -p"$PASSWD" -u"$USERNAME" "$DB" < SELECT COUNT(*) FROM globalize_translations g WHERE gtype="$2;
-  print select;
+done | awk -F "|" -v passwd=$PASS -v username=$USERNAME -v db=$DB '{
+  key = $1"|"$2
+  connect = "mysql -p"passwd" -u"username" "db" -e "
+  select = connect "\"SELECT * FROM globalize_translations g WHERE gtype=\x27"key"\x27\""
+  print select
+  # system(select)
+  select
+  # |& getline result
+  while ( ( select | getline result ) > 0 ) {
+    print result
+  } 
+  #close(select)  
+  # print result
+  # 1819: English
+  insert = connect "\"INSERT INTO globalize_translations VALUES (NULL,\x27\x27,NULL,1,\x27\x27,\x27"$3"\x27,\x27"$3"\x27,\x27"key"\x27,1819)\""
+  # print insert;
 }'
+
