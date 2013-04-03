acd76231205dbced7a78842c1fffd73c7a4b76e4
vjrj <vjrj@ourproject.org>
2013-04-03 12:59:17 +0200

    More work in i18n import

--- bin/i18n-wave2kune-import.sh
+++ bin/i18n-wave2kune-import.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 usage() {
-    echo "Use: $0 -u kune_db_username -p kune_db_passwd -d kune_db_name"
+    echo "Use: $0 -u kune_db_username -p kune_db_passwd -d kune_db_name -w path-to-wiab-code "
 }
 
+# This only for development
 USERNAME="kune"
 PASS="db4kune"
 DB="kune_dev"
-WAVE_HOME=/home/vjrj/dev/wave/
+WAVE_HOME=/home/vjrj/dev/wave/wiab/wiab-ro/
 
-while getopts “hu:p:d:” OPTION
+while getopts “hu:p:d:w:” OPTION
 do
     case $OPTION in
 	h)
@@ -25,6 +26,9 @@ do
 	d)
 	    DB=$OPTARG
 	    ;;   
+        w)
+	    WAVE_HOME=$OPTARG
+	    ;;
 	?)
             usage
             exit
@@ -32,7 +36,7 @@ do
     esac
 done	
 
-if [[ -z $PASS || -z $USERNAME || -z $DB ]]
+if [[ -z $PASS || -z $USERNAME || -z $DB || -z $WAVE_HOME ]]
 then
     usage
     exit 1
@@ -41,7 +45,7 @@ fi
 
 if [[ -z $WAVE_HOME ]]
 then 
-  echo "Please define were the Wave code is located using 'export WAVE_HOME=/some/path'"
+  echo "Please define were the Wave code is located using 'export WAVE_HOME=/some/path' or -w path option"
   exit 1
 fi
 
@@ -58,26 +62,28 @@ do
   sed "s/^/`echo $i | sed 's/\//\\\\\//g'`|/g" | \
   # Substitute first " = " with delimiter
   sed "0,/RE/s/ = /$DELIMITER/g" ; 
-done | gawk -F "|" -v passwd=$PASS -v username=$USERNAME -v db=$DB '
-# @include "mysql.awk"
+done | awk -F "|" -v passwd=$PASS -v username=$USERNAME -v db=$DB '
+function sql(operation) {
+   while ( ( operation | getline res ) > 0 ) {
+
+   } 
+   # print res
+   return res
+} 
 BEGIN {
-  # mysql_login(username, passwd, 'localhost', db)  
 }
-{ 
+{
   key = $1"|"$2
-  connect = "mysql -p"passwd" -u"username" "db" -e "
-  select = connect "\"SELECT * FROM globalize_translations g WHERE gtype=\x27"key"\x27\""
-  # print select
-  # system(select)
-  select 
-  # |& getline result
-  while ( ( select | getline result ) > 0 ) {
-    print result
-  } 
-  #close(select)  
-  # print result
+  connect = "mysql -p"passwd" -u"username" "db" --skip-column-names -e"
+  select = connect "\"SELECT count(*) FROM globalize_translations g WHERE gtype=\x27"key"\x27\""
+  #select 
+  result = sql(select)
+  if (result > 0) {
+    print "Already in db"
+  } else {
+    print "Dont exists, so insert"
+    insert = connect "\"INSERT INTO globalize_translations VALUES (NULL,\x27\x27,NULL,1,\x27\x27,\x27"$3"\x27,\x27"$3"\x27,\x27"key"\x27,1819)\""
+  }
   # 1819: English
-  insert = connect "\"INSERT INTO globalize_translations VALUES (NULL,\x27\x27,NULL,1,\x27\x27,\x27"$3"\x27,\x27"$3"\x27,\x27"key"\x27,1819)\""
-  # print insert;
 }'
 
