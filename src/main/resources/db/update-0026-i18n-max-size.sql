38587fbcbd1bae90dc96f43b7204622f66b4fdc7
vjrj <vjrj@ourproject.org>
2013-04-16 00:30:27 +0200

    Typo in db migration

--- src/main/resources/db/update-0026-i18n-max-size.sql
+++ src/main/resources/db/update-0026-i18n-max-size.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `kune_dev`.`globalize_translations` 
+ALTER TABLE globalize_translations 
   MODIFY COLUMN `text` VARCHAR(500),
   MODIFY COLUMN `tr_key` VARCHAR(500),
   MODIFY COLUMN `gtype` VARCHAR(500);
