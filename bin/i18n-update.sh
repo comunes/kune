120d6983f35c1c6883b7a1fbd629da1d5f3be909
vjrj <vjrj@ourproject.org>
2013-04-18 23:47:49 +0200

    Translations updated

--- bin/i18n-update.sh
+++ bin/i18n-update.sh
@@ -2,6 +2,7 @@
 
 HOST=kune@kune.beta.iepala.es
 
-rsync --partial bin/i18n* $HOST:/home/kune/kune-current/bin/
+rsync --partial bin/convertI18nMsgToMethods* $HOST:/home/kune/kune-current/bin/
+rsync --partial bin/i18n-* $HOST:/home/kune/kune-current/bin/
 ssh $HOST "cd /home/kune/kune-current/; bin/i18n-all.ssh"
 scp $HOST:/home/kune/kune-current/KuneCon* src/main/java/cc/kune/core/client/i18n/
