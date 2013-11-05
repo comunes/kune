f5b00818a55d1b8d7673ce9add3efd46eebd7bc3
vjrj <vjrj@ourproject.org>
2013-11-05 17:31:56 +0100

    Copyright added

--- bin/findFilesWithoutCopyright.sh
+++ bin/findFilesWithoutCopyright.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in `find src/main/java/cc/ src/test -name '*.java'`; do egrep -q "Copyright|Licensed to the Comunes Association|The kune development team|Licensed to the Apache Software" $i ; if [[ $? -ne 0 ]]; then echo $i ; fi; done
+for i in `find core/*/src/main/java/cc src/main/java/cc/ src/test -name '*.java'`; do egrep -q "Copyright|Licensed to the Comunes Association|The kune development team|Licensed to the Apache Software" $i ; if [[ $? -ne 0 ]]; then echo $i ; fi; done
