8f5dd2063f8b3530b849cf9072429452bf4bac25
pablojan <pablojan@pablojan@gmail.com>
2014-03-27 19:28:14 +0100

    Gadget sample upgrade to GWT 2.5.1

--- core/kune-java-gadget-lib/src/main/java/cc/kune/gadget/client/statecookie/StateLocalFactory.java
+++ core/kune-java-gadget-lib/src/main/java/cc/kune/gadget/client/statecookie/StateLocalFactory.java
@@ -21,8 +21,8 @@
 
 package cc.kune.gadget.client.statecookie;
 
-import com.google.gwt.autobean.shared.AutoBean;
-import com.google.gwt.autobean.shared.AutoBeanFactory;
+import com.google.web.bindery.autobean.shared.AutoBean;
+import com.google.web.bindery.autobean.shared.AutoBeanFactory;
 
 /**
  * A factory for creating StateLocal objects (are de/serialialized in a cookie).
