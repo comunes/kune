4befb01b14dfdd6e30528ab01594bc1140af4540
vjrj <vjrj@ourproject.org>
2017-09-15 15:44:02 +0200

    New version 1.0.2

--- bin/server.sh
+++ bin/server.sh
@@ -4,7 +4,7 @@
 
 usage() {
     echo "$0 [-j <jar file>] [-k <kune-config>] [-w <wave-config>] [-s <jaas config>] [Debug options] [other options]
-    Example: $0 -j target/kune-1.0.1-SNAPSHOT-jar-with-dependencies.jar -l IGNORE -d -p -u 20000 -o 8079 -y somekey
+    Example: $0 -j target/kune-1.0.2-SNAPSHOT-jar-with-dependencies.jar -l IGNORE -d -p -u 20000 -o 8079 -y somekey
 
 Options:
 -j <jar file> : runs jar file generated via mvn assembly:assembly
